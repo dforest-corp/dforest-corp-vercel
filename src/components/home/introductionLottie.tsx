167dbc7d247df1e1c920042bb4f5365c75c953b5
toda <ytoda@d-forest-corp.com>
2024-02-07 16:56:55 +0900

    画面外のlottieは停止する

--- src/components/home/introductionLottie.tsx
+++ src/components/home/introductionLottie.tsx
@@ -1,17 +1,32 @@
-import {memo} from 'react'
+import {memo, useRef} from 'react'
 import {Player} from '@lottiefiles/react-lottie-player'
+import {useInView} from 'react-intersection-observer'
 
 const IntroductionLottie = memo(() => {
+  const playerRef = useRef<Player>(null)
+  const [ref] = useInView({
+    onChange: (inView) => {
+      if (inView) {
+        playerRef.current?.play()
+      } else {
+        playerRef.current?.pause()
+      }
+    },
+  })
+
   return (
-    <Player
-      src="/lottie/intro.json"
-      loop
-      autoplay
-      rendererSettings={{
-        preserveAspectRatio: 'xMidYMid slice'
-      }}
-      className="cursor-default max-w-[1000px]"
-    />
+    <div ref={ref}>
+      <Player
+        ref={playerRef}
+        src="/lottie/intro.json"
+        loop
+        autoplay
+        rendererSettings={{
+          preserveAspectRatio: 'xMidYMid slice'
+        }}
+        className="cursor-default max-w-[1000px]"
+      />
+    </div>
   )
 })
 IntroductionLottie.displayName = 'IntroductionLottie'
--- src/components/home/treeLottie.tsx
+++ src/components/home/treeLottie.tsx
@@ -1,17 +1,29 @@
-import {memo} from 'react'
+import {memo, useRef} from 'react'
 import {Player} from '@lottiefiles/react-lottie-player'
+import {useInView} from 'react-intersection-observer'
 
 export type TreeLottieProps = {
   animationPath: any
 }
 
 const TreeLottie = memo(({animationPath}: TreeLottieProps) => {
+  const playerRef = useRef<Player>(null)
+  const [ref] = useInView({
+    onChange: (inView) => {
+      if (inView) {
+        playerRef.current?.play()
+      } else {
+        playerRef.current?.pause()
+      }
+    },
+  })
+
   return (
-    <div className={`aspect-square`}>
+    <div ref={ref} className={`aspect-square`}>
       <Player
+        ref={playerRef}
         src={animationPath}
         loop
-        autoplay
         rendererSettings={{
           preserveAspectRatio: 'xMidYMid meet'
         }}
