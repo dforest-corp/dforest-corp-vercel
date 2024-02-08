5e7090444a46675f814970bccb7297bc682df629
toda <ytoda@d-forest-corp.com>
2024-02-08 12:05:24 +0900

    リンクの表示修正

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,5 +1,18 @@
 import Link from 'next/link'
 import DropDownMenu from '../home/dropDownMenu'
+import {ReactNode} from 'react'
+
+function LinkItem({href, children}: {href: string, children: ReactNode}) {
+  return (
+    <li>
+      <Link
+        href={href}
+        className="text-sm font-bold relative ease-out duration-300 pb-0.5 after:origin-left after:absolute after:left-0 after:w-full after:h-px after:bottom-0 after:bg-black after:transition after:scale-x-0 hover:after:scale-x-100">
+        {children}
+      </Link>
+    </li>
+  )
+}
 
 const Header = () => {
   return (
@@ -9,21 +22,9 @@ const Header = () => {
           <Link href='/'>D-FOREST</Link>
         </h1>
         <ul className='hidden md:flex flex-row ml-auto space-x-6 text-lg text-gray-800'>
-          <li>
-            <Link href='/company'>
-              会社案内
-            </Link>
-          </li>
-          <li>
-            <Link href='/works'>
-              事業内容
-            </Link>
-          </li>
-          <li>
-            <Link href='/contacts'>
-              お問い合わせ
-            </Link>
-          </li>
+          <LinkItem href="/company">会社案内</LinkItem>
+          <LinkItem href="/works">事業内容</LinkItem>
+          <LinkItem href="/contacts">お問い合わせ</LinkItem>
         </ul>
         <DropDownMenu />
       </div>
