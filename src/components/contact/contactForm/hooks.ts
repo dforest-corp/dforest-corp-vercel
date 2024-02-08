2635ef7218341710d864d1d82d04980990b81f58
toda <ytoda@d-forest-corp.com>
2024-02-08 14:23:39 +0900

    zod -> valibot

--- src/components/contact/contactForm/hooks.ts
+++ src/components/contact/contactForm/hooks.ts
@@ -2,31 +2,48 @@ import {useEffect, useMemo} from 'react'
 import {useRouter} from 'next/navigation'
 import {toast} from 'react-toastify'
 import {useForm} from 'react-hook-form'
-import {zodResolver} from '@hookform/resolvers/zod'
+import {valibotResolver} from '@hookform/resolvers/valibot'
 import {useForm as useFormSpree} from '@formspree/react'
-import {z} from 'zod'
+import {email, Input, maxLength, minLength, object, string} from 'valibot'
 
-const formSchema = z.object({
-  name: z.string().min(1, 'お名前を入力してください。').max(60, '60文字以内で入力してください。'),
-  email: z.string().email('正しいメールアドレスを入力してください。'),
-  title: z.string(),
-  message: z.string().min(1, 'お問い合せ内容を入力してください。').max(4000, '4000文字以内で入力してください。')
+const formSchema = object({
+  name: string([
+    minLength(1, 'お名前を入力してください。'),
+    maxLength(60, '60文字以内で入力してください。'),
+  ]),
+  email: string([email('正しいメールアドレスを入力してください。')]),
+  title: string([maxLength(100, '100文字以内で入力してください。')]),
+  message: string([
+    minLength(1, 'お問い合せ内容を入力してください。'),
+    maxLength(4000, '4000文字以内で入力してください。'),
+  ]),
 })
 
-type FormSchemaType = z.infer<typeof formSchema>
+type FormSchemaType = Input<typeof formSchema>
 
 export const useContactFormHook = () => {
-  const [{submitting, succeeded, errors: sendError}, onSubmit] = useFormSpree(`${process.env.NEXT_PUBLIC_FORM_KEY}`)
-  const {register, handleSubmit, formState: {errors}} = useForm<FormSchemaType>({
-    resolver: zodResolver(formSchema)
+  const [{submitting, succeeded, errors: sendError}, onSubmit] = useFormSpree(
+    `${process.env.NEXT_PUBLIC_FORM_KEY}`,
+  )
+  const {
+    register,
+    handleSubmit,
+    formState: {errors},
+  } = useForm<FormSchemaType>({
+    resolver: valibotResolver(formSchema),
   })
   const router = useRouter()
 
-  const handleSubmitForm = useMemo(() => handleSubmit(onSubmit), [handleSubmit, onSubmit])
+  const handleSubmitForm = useMemo(
+    () => handleSubmit(onSubmit),
+    [handleSubmit, onSubmit],
+  )
 
   useEffect(() => {
     if (sendError) {
-      toast.error('お問い合わせを送信できませんでした。しばらく経ってから再度やり直してください。')
+      toast.error(
+        'お問い合わせを送信できませんでした。しばらく経ってから再度やり直してください。',
+      )
     }
   }, [sendError])
 
@@ -41,6 +58,6 @@ export const useContactFormHook = () => {
     register,
     handleSubmitForm,
     errors,
-    submitting
+    submitting,
   }
 }
