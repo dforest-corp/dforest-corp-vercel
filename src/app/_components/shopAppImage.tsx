5832b1d76a8075a0c641534eee90627e7930e3b4
toda <ytoda@d-forest-corp.com>
2024-02-08 17:59:40 +0900

    お店アプリの表示修正

--- src/app/_components/shopAppImage.tsx
+++ src/app/_components/shopAppImage.tsx
@@ -18,13 +18,16 @@ export function ShopAppImage() {
           inView && 'animate-fade-up-in',
         )}
       >
-        <Image
-          src="/images/shop-app.webp"
-          alt="ショップdeアプリ"
-          className="rounded-lg shadow-lg"
-          width={750}
-          height={500}
-        />
+        <div className="relative pb-4 pr-4">
+          <Image
+            src="/images/shop-app.webp"
+            alt="ショップdeアプリ"
+            className="relative z-10 rounded-2xl shadow"
+            width={750}
+            height={500}
+          />
+          <div className="absolute bottom-0 left-4 right-0 top-4 rounded-2xl bg-green-700" />
+        </div>
       </div>
     </div>
   )
