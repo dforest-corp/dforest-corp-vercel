835419f2ede3861c638388e7f6b836f1b1528f5b
toda <ytoda@d-forest-corp.com>
2024-02-09 10:09:18 +0900

    fix footer text

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <footer className="mt-auto bg-dforest-green py-20 text-white">
-      <p className="px-2 text-center">&copy; 2012 D-FOREST inc.</p>
+      <p className="px-2 text-center">&copy; 2012 D-FOREST Co., Ltd.</p>
     </footer>
   )
 }
