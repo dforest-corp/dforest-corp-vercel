ca2734576b2d5328497aa8d506d7b4efb5c7b719
toda <ytoda@d-forest-corp.com>
2024-02-13 14:36:08 +0900

    URLのリダイレクトを設定

--- next.config.js
+++ next.config.js
@@ -1,10 +1,25 @@
-const withBundleAnalyzer = process.env.ANALYZE === 'true'
-  ? require('@next/bundle-analyzer')({enabled: true})
-  : (config) => config;
+const withBundleAnalyzer =
+  process.env.ANALYZE === 'true'
+    ? require('@next/bundle-analyzer')({enabled: true})
+    : (config) => config
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/info/%E4%BA%8B%E6%A5%AD%E5%86%85%E5%AE%B9',
+        destination: '/works',
+        permanent: true,
+      },
+      {
+        source: '/info/%E4%BC%9A%E7%A4%BE%E6%A6%82%E8%A6%81',
+        destination: '/company',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = withBundleAnalyzer(nextConfig)
