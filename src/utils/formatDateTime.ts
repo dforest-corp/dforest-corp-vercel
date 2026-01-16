0b15efeb6ba153fe60cddeacee2aaddc912d0984
toda <ytoda@d-forest-corp.com>
2026-01-16 10:49:00 +0900

    upgrade libraries

--- src/utils/formatDateTime.ts
+++ src/utils/formatDateTime.ts
@@ -1,9 +1,8 @@
 import {Temporal} from 'temporal-polyfill'
 
 export const formatDateTime = (dateTime: string) => {
-  const targetTimeZone = Temporal.TimeZone.from('Asia/Tokyo')
   const instant = Temporal.Instant.from(dateTime)
-  const zoned = instant.toZonedDateTimeISO(targetTimeZone)
+  const zoned = instant.toZonedDateTimeISO('Asia/Tokyo')
   return zoned.toLocaleString('ja-JP', {
     year: 'numeric',
     month: '2-digit',
