6b1438de95b3edddf3e5e9ab7a381028d4c618b8
toda <ytoda@d-forest-corp.com>
2026-01-16 10:56:44 +0900

    add screen reader text to back to top button

--- src/components/backToTopButton.tsx
+++ src/components/backToTopButton.tsx
@@ -13,6 +13,7 @@ export function BackToTopButton() {
       onClick={handleClick}
       className="fixed bottom-4 right-4 flex h-12 w-12 items-center justify-center rounded-full border border-gray-200 bg-white shadow transition-opacity hover:bg-gray-200 lg:hidden"
     >
+      <span className="sr-only">一番上に戻る</span>
       <MdKeyboardArrowUp className="h-8 w-8 text-dforest-green" />
     </button>
   )
