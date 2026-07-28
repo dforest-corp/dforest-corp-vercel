93160cecb73c76aa7a7bbd5a459e5b41fc514748
toda <ytoda@dforest.jp>
2026-07-28 13:18:27 +0900

    update packages

--- src/app/contacts/_components/index.ts
+++ src/app/contacts/_components/index.ts
@@ -1,4 +1,4 @@
 /** @package */
 
 export {ContactTel} from '@/app/contacts/_components/contactTel'
-export {ContactForm} from 'app/contacts/_components/contactForm'
+export {ContactForm} from '@/app/contacts/_components/contactForm'
